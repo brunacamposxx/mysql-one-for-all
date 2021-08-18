e5dcf651edc797522c79fcf00b72fdcdf23c430e
Bruna Campos <92brunacampos@gmail.com>
2021-08-18 13:58:21 -0300

    erro de escrita no alias de seguindo para seguidores

--- desafio4.sql
+++ desafio4.sql
@@ -1,7 +1,7 @@
 
 CREATE VIEW SpotifyClone.top_3_artistas AS
 SELECT 
-a.artista AS artista, COUNT(s.artista_id) AS seguindo
+a.artista AS artista, COUNT(s.artista_id) AS seguidores
 FROM SpotifyClone.artista AS a
 INNER JOIN SpotifyClone.seguindo AS s
 ON a.artista_id = s.artista_id
