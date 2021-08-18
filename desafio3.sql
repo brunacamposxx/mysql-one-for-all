d2d09b37c90b77f4f322560f37ef7a61d0476c1d
Bruna Campos <92brunacampos@gmail.com>
2021-08-18 12:04:47 -0300

    code climate tabs

--- desafio3.sql
+++ desafio3.sql
@@ -1,10 +1,10 @@
 
 CREATE VIEW SpotifyClone.historico_reproducao_usuarios AS
 SELECT 
-	u.usuario as usuario, m.musica as nome
+u.usuario as usuario, m.musica as nome
 FROM SpotifyClone.historico as h
-  INNER JOIN SpotifyClone.usuario as u
-  ON u.usuario_id = h.usuario_id
-	INNER JOIN SpotifyClone.musica as m
-  ON h.musica_id = m.musica_id
+INNER JOIN SpotifyClone.usuario as u
+ON u.usuario_id = h.usuario_id
+INNER JOIN SpotifyClone.musica as m
+ON h.musica_id = m.musica_id
 ORDER BY usuario, nome;
