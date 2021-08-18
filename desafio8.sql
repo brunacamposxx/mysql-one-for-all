37359c80dbe1b60cc9d18dc4dd50d1fd41a03597
Bruna Campos <92brunacampos@gmail.com>
2021-08-18 15:50:04 -0300

    desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,10 @@
+DELIMITER $$
+CREATE TRIGGER SpotifyClone.trigger_usuario_delete
+BEFORE DELETE ON SpotifyClone.usuario
+FOR EACH ROW
+BEGIN
+    DELETE FROM SpotifyClone.seguindo WHERE usuario_id = OLD.usuario_id;
+    DELETE FROM SpotifyClone.historico WHERE usuario_id = OLD.usuario_id;
+END $$
+
+DELIMITER ;
