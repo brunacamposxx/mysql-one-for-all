faa00d4b958cda67776e5b338684235d543b240a
Bruna Campos <92brunacampos@gmail.com>
2021-08-18 16:26:45 -0300

    desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,18 @@
+DROP VIEW IF EXISTS cancoes_premium;
+CREATE VIEW SpotifyClone.cancoes_premium AS
+SELECT
+      m.musica AS nome,
+      COUNT(h.musica_id) AS reproducoes
+FROM
+      (SELECT
+            usuario_id
+      FROM
+            SpotifyClone.usuario
+      WHERE
+            plano_id IN (2, 3)) AS u
+INNER JOIN SpotifyClone.historico AS h ON u.usuario_id = h.usuario_id
+INNER JOIN SpotifyClone.musica AS m ON h.musica_id = m.musica_id
+GROUP BY 1
+ORDER BY 1;
+
+SELECT * FROM cancoes_premium;
